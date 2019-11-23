dc5c0d4d3fd59648975677899893728b14acb6bb
beach90710 <56116137+beach90710@users.noreply.github.com>
2019-11-23 18:32:20 +0900

    ajax-intro2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,19 +5,19 @@ $(function(){
     var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="message__text__image">`
     
     var html = `<div class="message" data-message-id="${ message.id }">
-                  <div class="message__upper-info">
-                    <div class="message__upper-info__talker">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
                       ${message.name}
                     </div>
-                    <div class="message__upper-info__date">
+                    <div class="upper-message__date">
                       ${message.created_at}
                     </div>
                   </div>
-                  <div class="message__text">
-                    <p class="message__text__content">
+                  <div class="lower-message">
+                    <p class="lower-message__content">
                       ${message.content}
                     </p>
-                    <div class="message__text__image">
+                    <div class="lower-message__image">
                     ${imagehtml}
                     </div>
                   </div>
@@ -28,7 +28,6 @@ $(function(){
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -40,16 +39,16 @@ $(function(){
       contentType: false,
     })
     .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('#new_message')[0].reset();
-      $('.form__submit').prop('disabled', false);
-　　 })
+        var html = buildHTML(data);
+        $('.messages').append(html);
+ 
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        $('#new_message')[0].reset();
+        $('.form__submit').prop('disabled', false);
+        return false;
+     })
     .fail(function() {
       alert('message error');
     })
-    return false;
   })
   })
